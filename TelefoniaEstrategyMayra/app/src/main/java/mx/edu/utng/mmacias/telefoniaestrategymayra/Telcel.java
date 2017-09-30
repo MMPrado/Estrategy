9a387040380ae72aa6e10db70ad175f1158bd22b
MMPrado <maciaspradomayra@gmail.com>
2017-09-29 20:24:14 -0500

    .
    .

---
+++ TelefoniaEstrategyMayra/app/src/main/java/mx/edu/utng/mmacias/telefoniaestrategymayra/Telcel.java
@@ -0,0 +1,19 @@
+package mx.edu.utng.mmacias.telefoniaestrategymayra;
+
+/**
+ * Created by Dell Latitude on 24/09/2017.
+ */
+
+public class Telcel implements Telefonica {
+
+
+    public double calcularTarifaLocal(int minLocal) {
+        return minLocal*2.0;
+    }
+
+
+    public double calcularTarifaLD(int minLD) {
+        return minLD*5.0;
+    }
+}
+
